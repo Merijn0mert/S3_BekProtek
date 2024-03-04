8666d635d97213832bd1c309a1d14c76fc696729
Merijn0mert <72080254+Merijn0mert@users.noreply.github.com>
2024-03-04 10:47:40 +0100

    Added backend java project

---
+++ Application/Backend/bekprotek/src/main/java/com/bekprotek/bekprotek/BekprotekApplication.java
@@ -0,0 +1,13 @@
+package com.bekprotek.bekprotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BekprotekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BekprotekApplication.class, args);
+	}
+
+}
---
+++ Application/Backend/bekprotek/src/test/java/com/bekprotek/bekprotek/BekprotekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekprotek.bekprotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekprotekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
